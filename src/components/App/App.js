8184ab7f4256302ecb2028e5d3824f930bb0d661
Joshua Smith <kazenostigma50@gmail.com>
2023-07-29 02:35:42 -0500

    added the start of the main section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,23 @@
-
-import Header from '../Header/Header';
-import './App.css';
+import Header from "../Header/Header";
+import "./App.css";
 
 function App() {
   return (
-   <>
-   <Header/>
-   </>
+    <>
+      <Header />
+      <main>
+        <section className="weather" id="weather">
+          <div className="weather__info">75 F</div>
+
+          <img
+            src="/images/day/sunny.svg"
+            alt="image of current weather"
+            className="weather__image"
+          />
+        </section>
+        <section id="card-section">Card Section</section>
+      </main>
+    </>
   );
 }
 
