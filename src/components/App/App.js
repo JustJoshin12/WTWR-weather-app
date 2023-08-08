266ae40506e9f4db4e2bb586090c6eb1daf16cf6
Joshua Smith <kazenostigma50@gmail.com>
2023-08-08 14:08:20 -0500

    reorganized and labeled code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+//                 Imports
+
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
@@ -6,15 +8,26 @@ import ItemModal from "../ItemModal/ItemModal";
 import AddItemModal from "../AddItemModal/AddItemModal";
 import { GetForecastWeather } from "../../utils/WeatherAPI/WeatherAPI";
 import { parseWeatherData } from "../../utils/WeatherAPI/WeatherAPI";
-
 import "./App.css";
 import { useState } from "react";
 import { useEffect } from 'react';
 
 function App() {
+    // Use States
   const [activeModal, setActiveModal] = useState("");
   const [selectedCard, setSelectedCard] = useState({});
   const [temp, setTemp] = useState(0);
+
+//  Use Effects
+
+  useEffect(() => {
+    GetForecastWeather().then((data) => {
+      const temperature = parseWeatherData(data);
+      setTemp(temperature);
+    })
+   }, [])
+
+   // Setter Functions
 
   const handleCreateModal = () => {
     setActiveModal("create");
@@ -29,18 +42,9 @@ function App() {
     setSelectedCard(card);
   };
 
- useEffect(() => {
-  GetForecastWeather().then((data) => {
-    const temperature = parseWeatherData(data);
-    setTemp(temperature);
-  })
- }, [])
-
-
-
   return (
     <div>
-      <Header onCreateModal={handleCreateModal} />
+      <Header onCreateModal={handleCreateModal} /> 
       <Main weatherTemp={temp} onSelectCard={handleSelectedCard} />
       <Footer />
       {activeModal === "create" && (
